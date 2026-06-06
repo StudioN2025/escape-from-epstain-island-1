15651e358f47db6b81cf1cc41d42c30c8de57584
MrDrist <abobachebaksar@gmail.com>
2026-06-06 13:38:16 +0300

    Create Player.js

---
+++ src/Player.js
@@ -0,0 +1,13 @@
+export class Player {
+    constructor(camera) {
+        this.camera = camera;
+        this.position = new THREE.Vector3(0, 1.6, 0);
+        this.sprint = false;
+        this.health = 100;
+    }
+    
+    updatePosition(deltaTime, moveVector) {
+        this.position.add(moveVector);
+        this.camera.position.copy(this.position);
+    }
+}
