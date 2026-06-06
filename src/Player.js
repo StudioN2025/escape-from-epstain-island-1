e846a6ccf875fd0bad3d3291739ea47861224f61
MrDrist <abobachebaksar@gmail.com>
2026-06-06 14:00:26 +0300

    Update Player.js

--- src/Player.js
+++ src/Player.js
@@ -4,8 +4,40 @@ export class Player {
     constructor(camera) {
         this.camera = camera;
         this.position = new THREE.Vector3(0, 1.6, 0);
+        this.velocity = new THREE.Vector3(0, 0, 0);
         this.sprint = false;
         this.health = 100;
+        this.isGrounded = true;
+        this.gravity = -15;
+        this.jumpPower = 7;
+    }
+    
+    updatePhysics(deltaTime, worldBounds) {
+        // Apply gravity
+        if (!this.isGrounded) {
+            this.velocity.y += this.gravity * deltaTime;
+            this.position.y += this.velocity.y * deltaTime;
+            
+            // Ground check
+            if (this.position.y <= 1.6) {
+                this.position.y = 1.6;
+                this.velocity.y = 0;
+                this.isGrounded = true;
+            }
+        }
+        
+        // Simple collision with world bounds
+        this.position.x = Math.max(worldBounds.minX, Math.min(worldBounds.maxX, this.position.x));
+        this.position.z = Math.max(worldBounds.minZ, Math.min(worldBounds.maxZ, this.position.z));
+    }
+    
+    jump() {
+        if (this.isGrounded) {
+            this.velocity.y = this.jumpPower;
+            this.isGrounded = false;
+            return true;
+        }
+        return false;
     }
     
     updatePosition(deltaTime, moveVector) {
