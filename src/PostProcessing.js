1eda6e2b14297630e9974172fcdbd9aac23ecf40
MrDrist <abobachebaksar@gmail.com>
2026-06-08 11:22:46 +0300

    Update PostProcessing.js

--- src/PostProcessing.js
+++ src/PostProcessing.js
@@ -10,8 +10,20 @@ export class PostProcessing {
         this.camera = camera;
         this.composer = null;
         this.bloomPass = null;
+        this.enabled = this.checkPerformance(); // Автоматическое отключение на слабых ПК
         
-        this.init();
+        if (this.enabled) {
+            this.init();
+        } else {
+            console.log('⚡ Постобработка отключена для производительности');
+        }
+    }
+    
+    checkPerformance() {
+        // Простая проверка производительности
+        // На слабых ПК отключаем эффекты
+        const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+        return !isMobile;
     }
     
     init() {
@@ -20,34 +32,35 @@ export class PostProcessing {
         const renderPass = new RenderPass(this.scene, this.camera);
         this.composer.addPass(renderPass);
         
-        // Приглушенное свечение
         this.bloomPass = new UnrealBloomPass(
             new THREE.Vector2(window.innerWidth, window.innerHeight),
-            0.35,   // strength
-            0.3,    // radius
-            0.15    // threshold
+            0.25, // Уменьшено с 0.35
+            0.2,
+            0.1
         );
         this.bloomPass.renderToScreen = true;
         this.composer.addPass(this.bloomPass);
         
-        console.log('✨ Постобработка настроена');
+        console.log('✨ Постобработка настроена (облегчённая)');
     }
     
     setBloomStrength(strength) {
-        if (this.bloomPass) {
+        if (this.bloomPass && this.enabled) {
             this.bloomPass.strength = strength;
         }
     }
     
     resize(width, height) {
-        if (this.composer) {
+        if (this.composer && this.enabled) {
             this.composer.setSize(width, height);
         }
     }
     
     render() {
-        if (this.composer) {
+        if (this.composer && this.enabled) {
             this.composer.render();
+        } else if (this.renderer) {
+            this.renderer.render(this.scene, this.camera);
         }
     }
 }
