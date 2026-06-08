13499cddf1df01df40c49de65fccaa07a7bce8e1
MrDrist <abobachebaksar@gmail.com>
2026-06-08 20:29:00 +0300

    Update StoryManager.js

--- src/StoryManager.js
+++ src/StoryManager.js
@@ -6,39 +6,36 @@ export class StoryManager {
         this.quests = {
             find_key: '🔑 Найдите ключ от двери в подвале',
             escape_basement: '🚪 Откройте дверь и выберитесь из подвала',
-            find_boat: '🛶 Найдите лодку на острове и сбегите от монстра'
+            find_boat: '🛶 Найдите лодку на острове',
+            find_fuel: '⛽ Найдите канистру с бензином на острове',
+            use_fuel: '🛶 Заправьте лодку и уплывите'
         };
     }
     
     startGame() {
-        this.addQuest('find_key', '🔑 Найдите ключ от двери в подвале');
-        if (this.ui) {
-            this.ui.updateQuest('Осмотрите подвал, возможно ключ где-то здесь...');
-        }
+        this.addQuest('find_key');
+        if (this.ui) this.ui.updateQuest('Осмотрите подвал, возможно ключ где-то здесь...');
     }
     
-    addQuest(questId, description) {
+    addQuest(questId, description = null) {
         this.currentQuest = questId;
         if (this.ui) {
-            this.ui.updateQuest(this.quests[questId] || description);
+            this.ui.updateQuest(this.quests[questId] || description || questId);
         }
     }
     
     completeQuest(questId) {
         if (this.currentQuest === questId) {
             const nextQuest = this.getNextQuest(questId);
-            if (nextQuest && this.ui) {
-                this.addQuest(nextQuest.id, nextQuest.description);
-            }
+            if (nextQuest) this.addQuest(nextQuest.id);
         }
     }
     
     getNextQuest(currentId) {
-        const questSequence = ['find_key', 'escape_basement', 'find_boat'];
-        const currentIndex = questSequence.indexOf(currentId);
-        if (currentIndex !== -1 && currentIndex < questSequence.length - 1) {
-            const nextId = questSequence[currentIndex + 1];
-            return { id: nextId, description: this.quests[nextId] };
+        const sequence = ['find_key', 'escape_basement', 'find_boat', 'find_fuel', 'use_fuel'];
+        const idx = sequence.indexOf(currentId);
+        if (idx !== -1 && idx < sequence.length - 1) {
+            return { id: sequence[idx + 1] };
         }
         return null;
     }
